55d0be60ee875eaf39c891299628c0a5b8621a0b
GurvGupta <gupta761951@gmail.com>
2022-10-07 01:02:39 +0530

    Create String Compression.cpp

---
+++ CPP/String Compression.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0;
+        int ansIndex=0;
+        int n=chars.size();
+        while(i<n){
+            int j=i+1;
+            while(j<n && chars[i]==chars[j]){
+                j++;
+            }
+            
+            chars[ansIndex++]=chars[i];
+            
+            int count=j-i;
+            
+            if(count>1){
+                string ch=to_string(count);
+                for(char c:ch){
+                    chars[ansIndex++]=c;
+                }
+            }
+            i=j;
+        }
+        
+        return ansIndex;
+    }
+};
