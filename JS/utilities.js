e32369703b708d6000132c499d835eff89e3b4c1
Akshat Mittal <akshatmittal2506@gmail.com>
2022-10-17 06:30:40 +0530

    Added navigator functions

--- JS/utilities.js
+++ JS/utilities.js
@@ -354,3 +354,42 @@ export const screenOrientationLock = () => screen.orientation.lock;
 // The function to get the screen orientation unlock
 
 export const screenOrientationUnlock = () => screen.orientation.unlock;
+// Some useful functions for the navigator
+
+// The function to get the navigator app code name
+
+export const navigatorAppCodeName = () => navigator.appCodeName;
+
+// The function to get the navigator app name
+
+export const navigatorAppName = () => navigator.appName;
+
+// The function to get the navigator app version
+
+export const navigatorAppVersion = () => navigator.appVersion;
+
+// The function to get the navigator cookie enabled
+
+export const navigatorCookieEnabled = () => navigator.cookieEnabled;
+
+// The function to get the navigator platform
+
+export const navigatorPlatform = () => navigator.platform;
+
+// The function to get the navigator product
+
+export const navigatorProduct = () => navigator.product;
+
+// The function to get the navigator user agent
+
+export const navigatorUserAgent = () => navigator.userAgent;
+
+// The function to get the navigator language
+
+export const navigatorLanguage = () => navigator.language;
+
+// The function to get the navigator languages
+
+export const navigatorLanguages = () => navigator.languages;
+
+// The function to get the navigator on line
