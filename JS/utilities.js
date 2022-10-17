7687075c46ef99e050094ca91290dbbe6e1eca4e
Akshat Mittal <akshatmittal2506@gmail.com>
2022-10-17 06:28:34 +0530

    Added window functions

--- JS/utilities.js
+++ JS/utilities.js
@@ -264,3 +264,44 @@ export const assert = (message) => console.assert(message);
 // The function to log a profile
 
 export const profile = (message) => console.profile(message);
+// Some useful functions for the window
+
+// The function to get the window inner width
+
+export const windowInnerWidth = () => window.innerWidth;
+
+// The function to get the window inner height
+
+export const windowInnerHeight = () => window.innerHeight;
+
+// The function to get the window outer width
+
+export const windowOuterWidth = () => window.outerWidth;
+
+// The function to get the window outer height
+
+export const windowOuterHeight = () => window.outerHeight;
+
+// The function to get the window scroll x
+
+export const windowScrollX = () => window.scrollX;
+
+// The function to get the window scroll y
+
+export const windowScrollY = () => window.scrollY;
+
+// The function to get the window scroll top
+
+export const windowScrollTop = () => window.scrollY;
+
+// The function to get the window scroll left
+
+export const windowScrollLeft = () => window.scrollX;
+
+// The function to get the window scroll width
+
+export const windowScrollWidth = () => window.scrollWidth;
+
+// The function to get the window scroll height
+
+export const windowScrollHeight = () => window.scrollHeight;
