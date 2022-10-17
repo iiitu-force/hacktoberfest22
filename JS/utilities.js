161244cf575552ea472747e9907ad3799c404adc
Akshat Mittal <akshatmittal2506@gmail.com>
2022-10-17 06:29:37 +0530

    Added screen functions

--- JS/utilities.js
+++ JS/utilities.js
@@ -305,3 +305,52 @@ export const windowScrollWidth = () => window.scrollWidth;
 // The function to get the window scroll height
 
 export const windowScrollHeight = () => window.scrollHeight;
+// Some useful functions for the screen
+
+// The function to get the screen width
+
+export const screenWidth = () => screen.width;
+
+// The function to get the screen height
+
+export const screenHeight = () => screen.height;
+
+// The function to get the screen avail width
+
+export const screenAvailWidth = () => screen.availWidth;
+
+// The function to get the screen avail height
+
+export const screenAvailHeight = () => screen.availHeight;
+
+// The function to get the screen color depth
+
+export const screenColorDepth = () => screen.colorDepth;
+
+// The function to get the screen pixel depth
+
+export const screenPixelDepth = () => screen.pixelDepth;
+
+// The function to get the screen orientation
+
+export const screenOrientation = () => screen.orientation;
+
+// The function to get the screen orientation type
+
+export const screenOrientationType = () => screen.orientation.type;
+
+// The function to get the screen orientation angle
+
+export const screenOrientationAngle = () => screen.orientation.angle;
+
+// The function to get the screen orientation onchange
+
+export const screenOrientationOnchange = () => screen.orientation.onchange;
+
+// The function to get the screen orientation lock
+
+export const screenOrientationLock = () => screen.orientation.lock;
+
+// The function to get the screen orientation unlock
+
+export const screenOrientationUnlock = () => screen.orientation.unlock;
